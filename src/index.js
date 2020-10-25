d32a2dbd6c06248a848ead3f9dcf577a5e9cb8b0
Kirill Nagorny <art.ngki@gmail.com>
2020-10-25 15:20:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openBrackets = [];
+    const closeBrackets = [];
+    bracketsConfig.map (bracketsPair => {
+        openBrackets.push (bracketsPair[0]);
+        closeBrackets.push (bracketsPair[1]);
+    });
+
+    let result = [];
+    for (let i of str) {
+        if (result[0] === i) {
+            result.shift();
+        } else if ((openBrackets.indexOf(i) > -1)) {
+            result.unshift(closeBrackets[openBrackets.indexOf(i)]);
+        } else {
+            return false;
+        }
+    }
+    return (result.length === 0);
 }
